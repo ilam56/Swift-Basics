c2fb2d38f29c74d0f72190b8965ad2a3dea7315a
Ian Anderson <ilam56@mail.missouri.edu>
2019-09-05 21:26:55 -0500

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Ian Anderson on 9/5/19.
+//  Copyright © 2019 Ian Anderson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
